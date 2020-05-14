d8f6a0a43519bdf9244bca6a3ac949c59bf2a467
meilick <Norman.Meilick@mail.schwarz>
2020-05-14 21:23:36 +0200

    First commit

---
+++ caller.go
@@ -0,0 +1,42 @@
+package rt
+
+import (
+	"fmt"
+	"runtime"
+)
+
+// CallerInfo holds info about a caller.
+type CallerInfo struct {
+	Name string
+	File string
+	Line int
+}
+
+func (ci *CallerInfo) String() string {
+	if ci == nil {
+		return "<unknown caller>"
+	}
+	return fmt.Sprintf("%s (%s:%d)", ci.Name, ci.File, ci.Line)
+}
+
+// NewCallerInfo creates a new CallerInfo initialized with the given values.
+func NewCallerInfo(name, file string, line int) *CallerInfo {
+	return &CallerInfo{
+		Name: name,
+		File: file,
+		Line: line,
+	}
+}
+
+// Caller returns caller info about the immediate parent caller of the
+// calling function, or nil if there's no info.
+func Caller() *CallerInfo {
+	pc := make([]uintptr, 1)
+	if runtime.Callers(3, pc) > 0 {
+		if f := runtime.FuncForPC(pc[0] - 1); f != nil {
+			file, line := f.FileLine(pc[0] - 1)
+			return NewCallerInfo(f.Name(), file, line)
+		}
+	}
+	return nil
+}
